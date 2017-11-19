79c541e0fd9801552f1398864f3224fa410bfe66
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-18 23:07:38 -0500

    Worked more on collatz

--- python/collatz.py
+++ python/collatz.py
@@ -4,15 +4,19 @@
 import sys
 import math
 
-x= int(input("Enter a number... "))  #Gets user input as an int type
-if x <= 1:
-      print "Invalid number"
-      sys.exit()
-count= 0
-while x != 1:
-      if x % 2 == 1:  #When odd number
-            x= (x * 3) + 1
-      else:  #When even number
-            x= x / 2
-      count+= 1
-      print "X = ", x, "   Sequence = ", count
+maxnum= 10000000
+largest= 1
+largecount= 1
+for i in range(2, maxnum):
+      x= i
+      count= 1
+      while x != 1:
+            if x % 2 == 1:  #When odd number
+                  x= (x * 3) + 1
+            else:  #When even number
+                  x= x / 2
+            count+= 1
+      if count > largecount:
+            largecount= count
+            largest= i
+      print largest, "   ", largecount
