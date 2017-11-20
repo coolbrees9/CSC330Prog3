3096d64366901ecbddb6575998e860a8aeef3378
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-19 20:12:55 -0500

    Fixed collatz w/out recursion

--- lisp/collatz.lisp
+++ lisp/collatz.lisp
@@ -1,17 +1,25 @@
 #!/usr/bin/sbcl --script
 ;This program will run Collatz conjecture
+(defvar maxnum 5000000000)
 (defvar x)
-(defvar counter 0)
-(write-line "Enter a number...")
-(setf x (read))  ;Gets user input
-(if (<= x 1)
-      (write-line "Invalid number")
-)
-(loop while (/= x 1) do
-      (if (eq (mod x 2) 1)  ;Odd number
-            (setf x (+ (* x 3) 1))
-            (setf x (/ x 2))  ;Even number
+(defvar counter)
+(defvar largest 1)
+(defvar largecount 1)
+(loop for i from 2 to maxnum do
+      (setf counter 1)
+      (setf x i)
+      (loop while (/= x 1) do
+            (if (eq (mod x 2) 1)  ;Odd number
+                  (setf x (+ (* x 3) 1))
+                  (setf x (/ x 2))  ;Even number
+            )
+            (setf counter (+ counter 1))
       )
-      (setf counter (+ counter 1))
-      (format t "X = ~,2d   Sequence = ~,2d ~%" x counter)
+      (if (> counter largecount)
+      (progn
+            (setf largecount counter)
+            (setf largest i)
+            (format t "~,2d   ~,2d ~%" largest largecount)
+      )
+      )
 )
