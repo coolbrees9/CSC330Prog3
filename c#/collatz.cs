710721d7379fd16a3fe542ad63f4ec9d44e9b58c
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-19 19:43:45 -0500

    Fixed collatz w/out recursion

--- c#/collatz.cs
+++ c#/collatz.cs
@@ -33,8 +33,8 @@ public class CollatzConjecture
                   {
                         largecount = count;
                         largest = i;
+                        Console.WriteLine(largest + "   " + largecount);
                   }
-                  Console.WriteLine(largest + "   " + largecount);
             }
       }
 }
