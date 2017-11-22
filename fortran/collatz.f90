c14a763811c9763fc644e0d64c0f4f429d63e06a
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-21 21:41:15 -0500

    Fixed collatz w/out recursion

---
+++ fortran/collatz.f90
@@ -0,0 +1,31 @@
+!This program will perform the Collatz conjecture
+program main
+      integer*8 :: maxnum
+      maxnum = 1000000000
+      call collatz(maxnum)  !Calls subroutine collatz
+end program main
+
+subroutine collatz(maxnum)
+      integer*8 :: maxnum
+      integer*8 :: largest
+      integer*8 :: i
+      integer*8 :: x
+      integer :: largecount, counter
+      do i = 2, maxnum
+            counter = 1
+            x = i
+            do while(x.ne.1)
+                  if((mod(x,2)) == 1) then
+                        x = (x * 3) + 1
+                  else
+                        x = x / 2
+                  end if
+                  counter = counter + 1
+            end do
+            if(counter > largecount) then
+                  largecount = counter
+                  largest = i
+                  print *, largest, "   ", largecount
+            end if
+      end do
+end subroutine collatz
