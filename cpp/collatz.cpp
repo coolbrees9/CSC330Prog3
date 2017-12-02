73fbfa16d1fe66b90a329877a733a323dfd1f9bb
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-12-02 00:20:28 -0500

    Finished normal collatz

--- cpp/collatz.cpp
+++ cpp/collatz.cpp
@@ -1,16 +1,27 @@
 //This program will perform the collatz conjecture
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+struct list
+{
+      long num;
+      long count;
+}csequence[10];
 
 static void collatz(long max)
 {
-      long largest = 1;
-      int largecount = 1;
-      for(long i = 2; i < max; i++)
+      while(max != 0)
       {
-            int count = 1;
-            long x = i;
+            long x = max;
+            bool duplicate = false;
+            long smallnum = 0;
+            long smallcount = csequence[0].count;
+            long index = 0;
+            long count = 0;
+            //Loop to do collatz
             while(x != 1)
             {
                   if(x % 2 == 1)
@@ -23,16 +34,47 @@ static void collatz(long max)
                   }
                   count++;
             }
-            if(count > largecount)
+            for(int i = 0; i < 10; i++)
             {
-                  largecount = count;
-                  largest = i;
-                  printf("%d   %d \n", largest, largecount);
+                  //Updates if current sequence greater than previous sequence
+                  if(smallcount > csequence[i].count)
+                  {
+                        smallcount = csequence[i].count;
+                        smallnum = i;
+                  }
+                  //Check for duplicate sequence
+                  if(csequence[i].count == count)
+                  {
+                        duplicate = true;
+                        index = i;
+                  }
             }
+            long temp = smallnum;
+            //No duplicates and adds in sequence if sequence greater than current 
+            if(count > csequence[temp].count && duplicate == false)
+            {
+                  csequence[temp].num = max;
+                  csequence[temp].count = count;
+            }
+            //If duplicates adds the smaller number
+            if(max < csequence[index].count && duplicate == true)
+                  csequence[index].num = max;
+            max--;
       }
+      printf("Sequence sorted by sequence length\n");
+      //Sort function to sort based on sequence length 
+      sort(csequence,csequence + 10,[] (list x, list y){return x.count > y.count;});
+      for(int j = 0; j < 10; j++)
+            printf("%d  %d \n", csequence[j].num, csequence[j].count);
+      printf("\n");
+      printf("Sequence sorted by integer size\n");
+      //Sort function that sorts based on num size
+      sort(csequence, csequence + 10, [](list x, list y){return x.num > y.num;});
+      for(int j = 0; j < 10; j++)
+            printf("%d  %d \n", csequence[j].num, csequence[j].count);
 }
 int main(int argc, char *argv[])
 {
-      long max = 5000000000;
+      long max = 10000;
       collatz(max);  //Calls collatz method to perform calculations
 }
