a505d7d92d956fbedd2de3e09a459aee56484f1f
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-12-03 16:02:25 -0500

    May need to add sorting

--- julia/collatz.jl
+++ julia/collatz.jl
@@ -1,24 +1,58 @@
 #Program will run Collatz conjecture
-maxnum = 1000000000
-i = Int32(2)
-counter = 0
-largest = 1
-largecount = 1
-for i = 2:maxnum
-  counter = 1
-  x = i
-  while ( x != 1 )
-      if  ((x % 2) == 1)  #Odd number
-      x = x * 3 + 1;
-      else  #Even number
-      x = x / 2;
+maxnum= 10000
+numlist = [0,0,0,0,0,0,0,0,0,0]
+countlist = [0,0,0,0,0,0,0,0,0,0]
+while (maxnum != 0)
+      x = maxnum
+      duplicate = "False"
+      smallnum = 1
+      smallcount = countlist[1]
+      index = 1
+      counter = 0
+      #Loop to do collatz
+      while (x != 1)
+            if (x % 2 == 1)  #When odd number
+                  x= (x * 3) + 1
+            else  #When even number
+                  x= x / 2
+            end 
+            counter+= 1
+     end
+     for i = 1:10
+            #Updates if current sequence greater than previous sequence
+            if(smallcount > countlist[i])
+                  smallcount = countlist[i]
+                  smallnum = i
+            end
+            #Check for duplicate sequence
+            if(countlist[i] == counter)
+                  duplicate = "True"
+                  index = i
+            end
       end
-      counter = counter + 1
-  end
-  if ( counter > largecount )
-      largecount = counter
-      largest = i
-      println( largest, "  ",  largecount )
-  end
+      temp = smallnum
+      #No duplicates and adds in sequence if sequence greater than current
+      if(counter > countlist[temp] && duplicate == "False")
+            numlist[temp] = maxnum
+            countlist[temp] = counter
+      end
+      #If duplicates adds the smaller number
+      if(maxnum < countlist[index] && duplicate == "True")
+            numlist[index] = maxnum
+      end
+      maxnum-=1
+end
+#println("Sequence sorted by sequence length")
+for j = 1:10
+      println(numlist[j],"  ", countlist[j])
+end
+println();
+#println("Sequence sorted by integer size")
+#Sort function that sorts based on num size
+#numlist.sort()
+#Sort function that sorts based on sequence size
+#countlist.sort()
+for j = 1:10
+      println(numlist[j],"  ", countlist[j])
 end
 exit(0)
