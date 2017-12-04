faf7833cbdd5c0ee6db6431a86996d34d59439e1
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-12-03 23:07:47 -0500

    May need to add sorting

--- ada/collatz.adb
+++ ada/collatz.adb
@@ -3,31 +3,65 @@ with Ada.Text_IO,Ada.IO_Exceptions,Ada.Integer_Text_IO,Ada.Float_Text_IO;
 use Ada.Text_IO,Ada.IO_Exceptions,Ada.Integer_Text_IO,Ada.Float_Text_IO;
 
 procedure collatz is
-max : long_Integer := 5000000000;
-largest : long_Integer := 1;
-largecount : Integer := 1;
-count : Integer;
-x : long_Integer;
+maxnum : long_Integer := 10000;
+numlist : array (1..10) of long_Integer; 
+countlist : array (1..10) of long_Integer;
+x, smallcount, counter : long_Integer;
+smallnum, indexnum, duplicate, temp : Integer;
 begin
-      for i in long_Integer range 2..max loop
-            count := 1;
-            x := i;
-            while(x > 1) loop
-                  if((x mod 2) = 1) then  --Odd number
+      for i in 1..10 loop
+            numlist(i) := 0;
+            countlist(i) := 0;
+      end loop;
+      while(maxnum /= 0) loop
+            x := maxnum;
+            duplicate := 0;
+            smallnum := 1;
+            smallcount := countlist(1);
+            indexnum := 1;
+            counter := 0;
+            --Loop that does collatz
+            while(x /= 1) loop
+                  if(x mod 2 = 1) then  --Odd number
                         x := (x * 3) + 1;
                   else  --Even number
                         x := x / 2;
                   end if;
-                  count := count + 1;
+                  counter := counter + 1;
             end loop;
-            if(count > largecount) then
-                  largecount := count;
-                  largest := i;
-                  Put(Item=> Long_Integer'Image(largest));
-                  Put("   ");
-                  Put(largecount);
-                  New_Line(1);
+            for i in 1..10 loop
+                  --Updates if current sequence greater than previous
+                  if(smallcount > countlist(i)) then
+                        smallcount := countlist(i);
+                        smallnum := i;
+                  end if;
+                  --Check for duplicates 
+                  if(countlist(i) = counter) then
+                        duplicate := 1;
+                        indexnum := i;
+                  end if;
+            end loop;
+            temp := smallnum;
+            --No duplicates and adds to the sequence
+            if(counter > countlist(temp) and duplicate = 0) then
+                  numlist(temp) := maxnum;
+                  countlist(temp) := counter;
             end if;
+            --If duplicate then add smaller number
+            if(maxnum < countlist(indexnum) and duplicate = 1) then
+                  numlist(indexnum) := maxnum;
+            end if;
+            maxnum := maxnum - 1;
       end loop;
-
+      for j in 1..10 loop
+            Put(Item=> Long_Integer'Image(numlist(j)));
+            Put("  ");
+            Put_Line(Item=> Long_Integer'Image(countlist(j)));
+      end loop;
+      Put_Line("");
+      for j in 1..10 loop
+            Put(Item=> Long_Integer'Image(numlist(j)));
+            Put("  ");
+            Put_Line(Item=> Long_Integer'Image(countlist(j)));
+      end loop;
 end collatz;
