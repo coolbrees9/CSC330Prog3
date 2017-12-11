5f45a00382ccf69456849a271fc18526f37e186d
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-12-11 12:41:02 -0500

    Added sorting method

--- ada/collatz.adb
+++ ada/collatz.adb
@@ -3,6 +3,9 @@ with Ada.Text_IO,Ada.IO_Exceptions,Ada.Integer_Text_IO,Ada.Float_Text_IO;
 use Ada.Text_IO,Ada.IO_Exceptions,Ada.Integer_Text_IO,Ada.Float_Text_IO;
 
 procedure collatz is
+
+swapNum : long_Integer;
+swapCount : long_Integer;
 maxnum : long_Integer := 10000;
 numlist : array (1..10) of long_Integer; 
 countlist : array (1..10) of long_Integer;
@@ -53,6 +56,40 @@ begin
             end if;
             maxnum := maxnum - 1;
       end loop;
+      --Loop that will bubblesort arrays
+      for x in 0..10 loop
+            for y in 1..9-x loop
+                  if(countlist(y) < countlist(y + 1)) then
+                        swapCount := countlist(y);
+                        countlist(y) := countlist(y + 1);
+                        countlist(y + 1) := swapCount;
+                        swapNum := numlist(y);
+                        numlist(y) := numlist(y + 1);
+                        numlist(y + 1) := swapNum;
+                  end if;
+            end loop;
+      end loop;
+      Put_Line("Sequence sorted by sequence length");
+      for j in 1..10 loop
+            Put(Item=> Long_Integer'Image(numlist(j)));
+            Put("  ");
+            Put_Line(Item=> Long_Integer'Image(countlist(j)));
+      end loop;
+      --Loop that will bubblesort arrays
+      for x in 0..10 loop
+            for y in 1..9-x loop
+                  if(numlist(y) < numlist(y + 1)) then
+                        swapNum := numlist(y);
+                        numlist(y) := numlist(y + 1);
+                        numlist(y + 1) := swapNum;
+                        swapCount := countlist(y);
+                        countlist(y) := countlist(y + 1);
+                        countlist(y + 1) := swapCount;
+                  end if;
+            end loop;
+      end loop;
+      Put_Line("");
+      Put_Line("Sequence sorted by integer size");
       for j in 1..10 loop
             Put(Item=> Long_Integer'Image(numlist(j)));
             Put("  ");
--- ada/recursivecollatz.adb
+++ ada/recursivecollatz.adb
@@ -14,6 +14,7 @@ begin
             return 1 + steps(x / 2);
       end if;
 end steps;
+swapNum, swapCount : long_Integer;
 maxnum : long_Integer := 10000;
 numlist : array (1..10) of long_Integer; 
 countlist : array (1..10) of long_Integer;
@@ -55,6 +56,53 @@ begin
             end if;
             maxnum := maxnum - 1;
       end loop;
+      --Loop that will bubblesort arrays
+      for x in 0..10 loop
+            for y in 1..9-x loop
+                  if(numlist(y) < numlist(y + 1)) then
+                        swapNum := numlist(y);
+                        numlist(y) := numlist(y + 1);
+                        numlist(y + 1) := swapNum;
+                        swapCount := countlist(y);
+                        countlist(y) := countlist(y + 1);
+                        countlist(y + 1) := swapCount;
+                  end if;
+            end loop;
+      end loop;
+      --Loop that will bubblesort arrays
+      for x in 0..10 loop
+            for y in 1..9-x loop
+                  if(countlist(y) < countlist(y + 1)) then
+                        swapCount := countlist(y);
+                        countlist(y) := countlist(y + 1);
+                        countlist(y + 1) := swapCount;
+                        swapNum := numlist(y);
+                        numlist(y) := numlist(y + 1);
+                        numlist(y + 1) := swapNum;
+                  end if;
+            end loop;
+      end loop;
+      Put_Line("Sequence sorted by sequence length");
+      for j in 1..10 loop
+            Put(Item=> Long_Integer'Image(numlist(j)));
+            Put("  ");
+            Put_Line(Item=> Long_Integer'Image(countlist(j)));
+      end loop;
+      --Loop that will bubblesort arrays
+      for x in 0..10 loop
+            for y in 1..9-x loop
+                  if(numlist(y) < numlist(y + 1)) then
+                        swapNum := numlist(y);
+                        numlist(y) := numlist(y + 1);
+                        numlist(y + 1) := swapNum;
+                        swapCount := countlist(y);
+                        countlist(y) := countlist(y + 1);
+                        countlist(y + 1) := swapCount;
+                  end if;
+            end loop;
+      end loop;
+      Put_Line("");
+      Put_Line("Sequence sorted by integer size");
       for j in 1..10 loop
             Put(Item=> Long_Integer'Image(numlist(j)));
             Put("  ");
